5da858e4e25326748dd29871901fc93d932c89d9
Vlad Tsurko <chegnenago@yahoo.com>
2019-06-28 19:21:39 +0300

    Added some self-explanatory code to "baconIpsum" JSONP demo.

--- baconIpsum/baconIpsum.js
+++ baconIpsum/baconIpsum.js
@@ -1,17 +1,23 @@
 /*global $ */
 $(document).ready(function () {
     $("#backonButton").click(function() {
-        $.getJSON("https://baconipsum.com/api/?callback=?", {
-            "type": "meat-and-filler",
-            "start-with-lorem": "1",
-            "paras": "3"
-        },
-                  function(baconGoodness) {
-                      if(baconGoodness && baconGoodness.length > 0) {
-                          $("#baconIpsumOutput").html("");
-                          for(var i = 0; i < baconGoodness.length; i++)
-                              $("#baconIpsumOutput").append("<p>" + baconGoodness[i] + "</p>");
-                      }
-                  });
+        $.ajax({
+            url: "https://baconipsum.com/api/?callback=?",
+            data: {
+                "type": "meat-and-filler",
+                "start-with-lorem": "1",
+                "paras": "3"
+            },
+            dataType: "json",
+            jsonpCallback: "bulki"
+        });
     });
 });
+
+function bulki(baconGoodness) {
+    if(baconGoodness && baconGoodness.length > 0) {
+        $("#baconIpsumOutput").html("");
+        for(var i = 0; i < baconGoodness.length; i++)
+            $("#baconIpsumOutput").append("<p>" + baconGoodness[i] + "</p>");
+    }
+}
