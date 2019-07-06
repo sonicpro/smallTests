ce90518d8a0cfafc6c1fedfaba1fd515f9bc2125
Volodymyr Tsurko <sonicpro@ukrpost.ua>
2019-07-06 13:53:12 +0300

    "ng-repeat" added to AngularJS Essentials "Parking" demo.

---
+++ AngularJS Essentials/app.js
@@ -0,0 +1,15 @@
+var app = angular.module("app", [])
+    .controller("parkingCtrl", ["$scope", parkingCtrl]);
+
+function parkingCtrl($scope) {
+    $scope.cars = [
+        { plate: "6MBV006" },
+        { plate: "5BBM299" },
+        { plate: "5AOJ230" }
+    ];
+
+    $scope.park = function (car) {
+        $scope.cars.push(car);
+        delete $scope.car;
+    }
+}
