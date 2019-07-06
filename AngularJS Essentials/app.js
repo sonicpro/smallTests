99f9c527c91a365b74c585c8cb8b884562fe54a4
Volodymyr Tsurko <sonicpro@ukrpost.ua>
2019-07-06 15:43:42 +0300

    Two approaches to passing data from the view to controller actions.

--- AngularJS Essentials/app.js
+++ AngularJS Essentials/app.js
@@ -10,7 +10,16 @@ function parkingCtrl($scope, $sce) {
 
     $scope.appTitle = $sce.trustAsHtml("<b>[Packt] Parking</b>");
 
+    // High coupling version of the park() controller action.
+    // $scope.park = function () {
+    //     $scope.car.entrance = new Date();
+    //     $scope.cars.push($scope.car);
+    //     delete $scope.car;
+    // }
+
+    // Low coupling version.
     $scope.park = function (car) {
+        car.entrance = new Date();
         $scope.cars.push(car);
         delete $scope.car;
     }
