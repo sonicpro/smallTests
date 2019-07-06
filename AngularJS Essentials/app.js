17f12c56a3f09a9b7a28fc7b1e4ffd93eedf83c2
Volodymyr Tsurko <sonicpro@ukrpost.ua>
2019-07-06 15:17:58 +0300

    "ng-bind-html" directive and "ng-sanitize" module added.

--- AngularJS Essentials/app.js
+++ AngularJS Essentials/app.js
@@ -1,12 +1,14 @@
-angular.module("app", [])
-    .controller("parkingCtrl", ["$scope", parkingCtrl]);
+angular.module("app", ["ngSanitize"])
+    .controller("parkingCtrl", ["$scope", "$sce", parkingCtrl]);
 
-function parkingCtrl($scope) {
+function parkingCtrl($scope, $sce) {
     $scope.cars = [
         { plate: "6MBV006" },
         { plate: "5BBM299" },
         { plate: "5AOJ230" }
     ];
+
+    $scope.appTitle = $sce.trustAsHtml("<b>[Packt] Parking</b>");
 
     $scope.park = function (car) {
         $scope.cars.push(car);
