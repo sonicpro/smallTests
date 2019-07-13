75530a7c0e6868ea510a876915c984d48057b5f3
Volodymyr Tsurko <sonicpro@ukrpost.ua>
2019-07-13 16:43:37 +0300

    AngularJS Essentials moved into their own repository.

--- AngularJS Essentials/Ch4/filters/plate.js
+++
@@ -1,8 +0,0 @@
-// Registring a new filter.
-app.filter("plate", function () {
-    return function (value) {
-        var firstPart = value.slice(0, 3),
-            secondPart = value.slice(3);
-        return firstPart + "-" + secondPart;
-    };
-});
